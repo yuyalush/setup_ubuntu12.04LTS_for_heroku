4056fed87f0caf993df0f80460c0197a19afd08e
Yuya Yoshida <yoshidayuya@gmail.com>
2012-08-17 03:01:54 +0900

    Update setup_ruby.sh

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 # Setup for git , Ruby1.9.2 , Rails3.2.8
 
-echo 'git'
-sudo apt-get -y install git
+echo 'Install libs'
+sudo apt-get -y install git build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev libsqlite3-dev g++ 
 
-echo 'libs'
-sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev libsqlite3-dev g++ 
-# ruby1.9.1-dev
-
-echo 'rbenv'
+echo 'Install rbenv'
 sudo apt-get -y install rbenv
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 eval "$(rbenv init -)"
 source ~/.bashrc
 
-echo 'ruby-build'
+echo 'Install ruby-build'
 git clone git://github.com/sstephenson/ruby-build.git
 cd ruby-build
 sudo ./install.sh
 cd ..
 
-# install Ruby 1.9.2-p320
-echo 'install Ruby1.9.2-p320'
+echo 'Install Ruby1.9.2-p320'
 rbenv install 1.9.2-p320
 rbenv rehash
 rbenv global 1.9.2-p320
 echo 'gem: --no-ri --no-rdoc' >> ~/.gemrc
 
+echo 'Install Rails & Generate testapp'
 gem install rails -v=3.2.8
 gem install foreman
 rbenv rehash
+
 rails new testapp --skip-bundle
 cd testapp
 echo "gem 'thin'" >> Gemfile
