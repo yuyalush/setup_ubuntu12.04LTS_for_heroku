1a73940c68a37e1a93634f633275e33c11b4ee00
yuyalush <yoshidayuya@gmail.com>
2012-08-17 01:14:28 +0900

    create setup_rails.sh

---
+++ setup_rails.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# source ~/.bashrc
+
+sudo gem install rails -v=3.2.8
+sudo gem install foreman
+rbenv rehash
+rails new testapp --skip-bundle
+cd testapp
+echo "gem 'thin'" >> Gemfile
+echo "web: bundle exec rails server thin -p 3000 -e development" >> Procfile
+sudo bundle install
+
+rails g scaffold Book title:string price:integer
+rake db:migrate
+foreman start
+
